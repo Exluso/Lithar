e354abd9b0013650d59db70cf7817e8ff7cb86f9
Exluso <pg.miceli@gmail.com>
2020-01-15 00:26:47 +0100

    gamma check for previous archive

--- lithar_s.py
+++ lithar_s.py
@@ -2,28 +2,20 @@
 """
 Created on Thu Jan  9 14:53:15 2020
 
+The program backs up an entire folder (i.e. all it's contents: file and subfolders) in a .zip archive.
+If there is already a .zip archive with the same format used by the program, it will update
+by overwriting the most recent version (if present) of each file.
+Files without an update will be skipped.
+
 @author: Exluso
 """
 
 import os, logging, shutil, re, sys
 
 logging.basicConfig(level = logging.DEBUG, format = "%(asctime)s - %(levelname)s - %(message)s")
-#logging.disable(logging.CRITICAL)
-
-logging.debug("Starts in cwd:" + os.getcwd())
-
-def matchArchive(testName, CoreName):
-    #looks for .zip archive of with the same CoreName of the target folder.
-    # CoreName: a str with the name of the original folder
-
-    #TODO: vedi se funge
-    
-    LithArchivePat = re.compile(CoreName + r"\d*.zip")
-    if LithArchivePat.search(testName) != None:
-        return True
-    else:
-        return False
-    
+#logging.disable(logging.CRITICAL) #uncomment to remove logging
+logging.debug("Starts in cwd: " + os.getcwd())
+ 
 #The program starts and gets the path of the target folder (i.e. the one that 
 #needs to be zipped and the path where to store the .zip archive
 #it also gets the root for the name of the .zip archive from the target folder
@@ -34,31 +26,37 @@ try:
     masterLines= masterFile.readlines()
 
 except:
-    print("""Non è stato trovato alcun LitharMaster.txt file.
+    print("""\nNon è stato trovato alcun LitharMaster.txt file.
     Per permettere al programma di funzionare crea LitharMaster.txt
     nella stessa folder del programma inserendo:
     1 riga il path della folder da zippare
-    2 riga path dove creare l'archivio """)
+    2 riga path dove creare l'archivio \n """)
     sys.exit()
     
     #ToDo: crea una funzione per far creare il file direttamente al programma 
     # chiedendo all'utente i percorsi necessari.
 
-logging.debug("Sorgente: %s" %(masterLines[0]))
+logging.debug("Sorgente: %s" %(masterLines[0][:-1]))
 logging.debug("Destinazione: %s" %(masterLines[1]))
 
-print("BENVENUTO IN LITHAR.")
+print("\nBENVENUTO IN LITHAR.")
 print("L'oggetto del tuo back up è:\n %s." %(masterLines[0][:-1]))
-print("Il backup verrà conservato in:\n %s." %(masterLines[1]))
+print("Il backup verrà conservato in:\n %s. \n" %(masterLines[1]))
 
-newZipCoreName = os.path.basename(masterLines[0])
+#checks if there is already a .zip file made by Lithar
+
+newZipCoreName = os.path.basename(masterLines[0][:-1])
 
 logging.debug("New Zip Core Name: %s" %(newZipCoreName))
+logging.debug("dir files: %s" %(os.listdir(masterLines[1])))
 
 for fileName in os.listdir(masterLines[1]):
-    logging.debug("testing now: " + fileName)    
-    if matchArchive(fileName, newZipCoreName): #fileName.endswith(".zip"):
-        print("C'è già un archivio zip: %s" %(fileName))
-        com = input("Vuoi aggiornarlo? (a) \nVuoi crearne uno nuovo? (n)\n")
-        
+    #ToDO: fai in modo che il loop ritorni una lista degli archivi precedenti solo alla fine del loop
+    logging.debug("testing now: %s " %(fileName))
+    logging.debug("matchArchive: %s" % str(re.search(newZipCoreName + r'\d*' + ".zip", fileName)))
+    if re.search(newZipCoreName, fileName) is not None:
+        print("È stato trovato un archivio con lo stesso nome: %s."  %(fileName))
+    else:
+        print("Non ci sono archivi precedenti. Ne creo uno.") # Placheholder
+
 
