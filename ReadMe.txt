90d13ec111f166332b151001ddb31b30be72f263
Exluso <pg.miceli@gmail.com>
2020-01-22 22:57:23 +0100

    refactoring2 - checkFile(), better createBak(), better user propmt

--- ReadMe.txt
+++ ReadMe.txt
@@ -2,28 +2,37 @@ Lithar è un programma di backup.
 Funzioni desiderate:
 - Creare il backup di tutti i contenuti di una folder (Target), incluse le subfoldere i loro contenuti fino alle ultime ramificazioni.
 - Aggiornare un backup pre-esistente della folder Target, aggiornando soltanto i file che sono stati modificati.
-- Probabilmente lo status modificato verrà valutato in base alle data di ultima modifica del file.
+- Probabilmente lo status modificato verrà valutato in base alle data di ultima modifica del file Vs data di modifica del file in backup.
+- Creare un archivio del backup
 
 
 PARTI:
 
-PART I (done)
--Benvenuto e controllo della situazione.
--Lithar riceve la sorgente dei dati originali e la destinazione in cui salvare il backup
--Lithar chiede se si vuole fare un l'archivio .zip del backup
+PART I
+-Benvenuto e controllo della situazione. --DONE--
+-Lithar riceve la sorgente dei dati originali e la destinazione in cui salvare il backup --DONE--
+-Lithar mostra eventuali backup fodlers e archivi zip già presenti --DONE--
+-- mostra la data di creazione del back dopo che si inserisce la data nel nome.
+-Lithar chiede che azioni fare:
+-- creare un nuovo backup --DONE--
+--- creare un nuovo backup con la data di creazione nel nome della folder.
+-- creare un archivio .zip (sempre dell'originale) --DONE--
 -- Mostra l'elenco degli Archivi precedenti e il timestamp della loro ultima modifica.
 
 PART II (ToDo)
-Lithar offre di creare/aggiornare archivi
+Lithar offre di aggiornare folder di backup
 
 ToDo Core:
--Funzione createArc() che crea un archivio
+-Funzione createArc() che crea un archivio (--DONE--)
+-Funzione createBak() crea l'intero backup semplicemente copincollando le folder --DONE--
+-Funzione checkFile() controlla che folder di backup e archivi .zip esistono già --DONE--
+-Funzione updateBak() aggiorna una folder di backup sovrascrivendo i file con modifica più recente.
 
--Funzione updateArc() aggiorna un archivio esistente sovrascrivendo i file modificati. --ABANDONED--
+-Funzione updateArc() aggiorna un archivio .zip esistente sovrascrivendo i file modificati. --ABANDONED--
 
-ToDo Optional Refactoring
+ToDo Optional e Refactoring
 -cambiare il formato della data dell'ultima modifica (ad es in PART I)
--Funzione acquirePath(): modularizzare la funzione per prendere i path sorgente e destinazione
---Funzione cleanPath(): prende i path da masterFile e rimuove il linebreak alla fine (se presente).
----- cambiare nel codice le occorrenze di masterLine[0][:-1] ad una variabile col path generato da cleanPath()
+-Funzione acquirePath(): modularizzare la funzione per prendere i path sorgente e destinazione --DONE--
+--Funzione cleanPath(): prende i path da masterFile e rimuove il linebreak alla fine (se presente). --DONE--
 -Funzione createMaster(): crea il Masterfile da cui prendere i path sorgente e destinazione
+-file .bin con metadati sulle date di creazione? goosh
--- lithar_s.py
+++ lithar_s.py
@@ -47,10 +47,34 @@ def acquirePath(file):
 def createBak(source, dest):
     ''' creates a new directory source_bakXX where the source is copied.
     XX is a serial number, in case of more than 1 backups'''
-    #ToDo implement a proper serial number
-    serialN = "01" #placeholder
-    shutil.copytree(source, dest + os.sep + os.path.basename(source)+ "_bak"+serialN)
+    
+    lastBak = 0
+    for folder in bakList:
+        serialN = int(folder.lstrip(baseFileName+"_bak"))
+        if serialN > lastBak:
+            lastBak = serialN
 
+    shutil.copytree(source, dest + os.sep + os.path.basename(source)+ "_bak"+ str(lastBak+1))
+
+def checkForFile(tipo = "bak"):
+    '''returns a list of the specific file or folder in the bakPath directory
+    2nd argument: "zip" to check for .zip archives, "bak" to check for backup fodler'''
+    fileList = []
+    if tipo == "bak":
+        pat = re.compile(baseFileName + "_bak" +r'\d*')
+    elif tipo == "zip":
+        pat = re.compile(baseFileName + r'\d*' + ".zip")
+    else:
+        print("ATTENZIONE TIPO DI FILE NON SPECIFICATO!")
+    
+    for thing in os.listdir(bakPath):
+       
+        logging.debug("testing now: %s " %(thing))
+        logging.debug("matchArchive: %s" % str(pat.search(thing)))   
+        if pat.search(thing) is not None:
+            fileList.append(thing)
+
+    return fileList
 
 def createArc(dest, name):
     ''' Creates a .zip Archive named with a sequential number that follows the greater numbered name already in the folder
@@ -60,26 +84,26 @@ def createArc(dest, name):
     logging.debug("name: %s" %(name))
     #Create archive
     lastArch = 0
-    try:
-        for arch in archList:
-            serialNum = int(arch.lstrip(name).rstrip(".zip"))
-            logging.debug("Current Serial: %d" % (serialNum))
-            if serialNum > lastArch:
-                lastArch = serialNum
-        logging.debug("lastArch finale: {}".format(lastArch))
-        
-        newArc = zipfile.ZipFile(dest + os.sep + name + str(lastArch+1)+".zip","w")
+    #try:
+    for arch in archList:
+        serialNum = int(arch.lstrip(name).rstrip(".zip"))
+        logging.debug("Current Serial: %d" % (serialNum))
+        if serialNum > lastArch:
+            lastArch = serialNum
+    logging.debug("lastArch finale: {}".format(lastArch))
+    
+    newArc = zipfile.ZipFile(dest + os.sep + name + str(lastArch+1)+".zip","w")
     # adds file to archive
-        for curFolder, subFolders, fileNames in os.walk(os.path.basename(source)):
-            for fileName in fileNames:
-                savingPath = os.path.join(curFolder, fileName)
-                newArc.write(savingPath, compress_type=zipfile.ZIP_DEFLATED)
+    for curFolder, subFolders, fileNames in os.walk(os.path.basename(original)):
+        for fileName in fileNames:
+            savingPath = os.path.join(curFolder, fileName)
+            newArc.write(savingPath, compress_type=zipfile.ZIP_DEFLATED)
         
-    except:
-        print("Qualcosa è andato storto. Verifica che il nome degli archivi già presenti sia come il seguente:")
-        print('"Nome della folder originale" + numero seriale + ".zip"')
-    finally:
-        newArc.close()
+    # except:
+    #     print("Qualcosa è andato storto. Verifica che il nome degli archivi già presenti sia come il seguente:")
+    #     print('"Nome della folder originale" + numero seriale + ".zip"')
+    # finally:
+    newArc.close()
 
 logging.basicConfig(level = logging.DEBUG, format = "%(asctime)s - %(levelname)s - %(message)s")
 #logging.disable(logging.CRITICAL) #uncomment to remove logging
@@ -93,6 +117,8 @@ logging.debug("Starts in cwd: " + os.getcwd())
 #LitharMaster.txt is where the path are stored.
 
 original , bakPath = acquirePath("MasterFile.txt") #path of original folder, path where to create the backup folder
+baseFileName = os.path.basename(original)
+
 logging.debug("Sorgente: %s" %(original))
 logging.debug("Destinazione: %s" %(bakPath))
 
@@ -102,36 +128,44 @@ print("Il backup verrà conservato in:\n %s. \n" %(bakPath))
 
 # Checks if there are previous archives and prints them in order
 
-newZipCoreName = os.path.basename(original)
-
-logging.debug("New Zip Core Name: %s" %(newZipCoreName))
+logging.debug("New Zip Core Name: %s" %(baseFileName))
 logging.debug("dir files: %s" %(os.listdir(bakPath)))
 
-archList =[]
-for fileName in os.listdir(bakPath):
-    #ToDO: prova a cambiare il formato della data, avendo l'anno prima dell'ora
-    logging.debug("testing now: %s " %(fileName))
-    logging.debug("matchArchive: %s" % str(re.search(newZipCoreName + r'\d*' + ".zip", fileName)))    
-    if re.search(newZipCoreName, fileName) is not None:
-        archList.append(fileName)
-    logging.debug(archList)
+archList =checkForFile("zip")
+bakList = checkForFile("bak")
 
-if len(archList)>0:
+logging.debug("baklist: %s" %(bakList))
+logging.debug("archList: %s" %(archList))
+
+#Shows backup folder and zip archives already present
+if len(bakList)>0 or len(archList)>0 :
+     #ToDO: prova a cambiare il formato della data, avendo l'anno prima dell'ora
     print("Ho trovato i seguenti archivi che potrebbero contenere backup precedenti: \n")
-    print("#".ljust(3) + "ARCHIVIO".ljust(15) + "ULTIMA MODIFICA".ljust(30))
+    print("#".ljust(4) + "ARCHIVIO".ljust(15) + "ULTIMA MODIFICA".ljust(30))
+    for folder in bakList:
+        lmdEpoch = os.path.getmtime(bakPath+os.sep+folder)
+        lmd = time.ctime(lmdEpoch)
+        print((str(bakList.index(folder))+ ") ").center(4," ") + (folder).ljust(15) + str(lmd).ljust(30))
+        
     for arc in archList:
         lmdEpoch = os.path.getmtime(bakPath+os.sep+arc) #return the "last modified" date (hopefully)
         lmd = time.ctime(lmdEpoch)
-        print((str(archList.index(arc))+ ") ").center(3) + (arc).ljust(15) + str(lmd).ljust(30))
-    #ToDo: offer to update an archive or create a new one
-    print("Per aggiornare un archivio, immetti il numero corrispondente.")
-    choice = input("\nPer creare un nuovo backup digita \"b\", per un archivio digita \"n\".\n")
-else:
-    #ToDo: it there are no archives, offer to create one
-    print("Non ci sono backup. Digita \"b\" per crearne uno.")
-    choice = input("Non ci sono archivi precedenti. Digita \"z\" per crearne uno nuovo. \n") # placeholder
+        print(("z"+str(archList.index(arc))+ ") ").center(4) + (arc).ljust(15) + str(lmd).ljust(30))
+ 
+if len(bakList) < 1:
+    print("Non sono state trovate folder di backup precedenti.")
+if len(archList) <1:
+    print("Non sono stati trovati archivi .zip precedenti.")
 
-#ToDo: use assert/exception to check that the number of the archive is not greater than len(archList)
+#Lithar offers to create a backup, to update a backup, to create an archive ToDo: offer to update an archive
+#ToDo: assegna i comandi a delle variabili e usale sia nelle stringhe che negli "if"
+print()
+print("Digita \"b\" per creare una nuova folder di backup.")
+print("Digita \"aggiorna\" seguito dal numero del backup per aggiornare il backup (es. aggiorna 1).")
+print("Digita \"z\" per creare un nuovo archivio .zip")
+#ToDo: use assert/exception to check that the number of the "aggiorna" option is not greater than len(bakList).
+
+choice = input("Inserisci il comando corrispondente alla tua scelta: \n")
 
 if choice == "b":
     createBak(original, bakPath)
